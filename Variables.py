ab5a6b15939bbe564835a7719e8bf96b13a3a9e7
Munuve30 <77729985+Munuve30@users.noreply.github.com>
2021-02-11 11:14:19 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,27 @@
+#variables in terms of integers,floats & strings
+age=30
+salary=34500.6785
+name="Joshua"
+
+print(age)
+print(salary)
+print(name)
+
+ #different values to multiple variables
+a, b, c = 4300, 25.7, "Joshpin"
+print(a)
+print(b)
+print(c)
+
+#1 value to different variables
+a = b = c = 20
+print(a)
+print(b)
+print(c)
+
+#same name for different types
+a = 30
+a = "Joshua"
+print(a)
+#last name is the one recognized
+ 
